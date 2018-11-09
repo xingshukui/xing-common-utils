ea09d048f8f6095aff7d7863ffaca09d03a31132
xingshukui <xingshukui@163.com>
2018-11-09 14:43:04 +0800

    注册bean

--- src/main/java/com/xing/utils/spring/SpringUtils.java
+++ src/main/java/com/xing/utils/spring/SpringUtils.java
@@ -1,8 +1,13 @@
 package com.xing.utils.spring;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
@@ -34,4 +39,21 @@ public class SpringUtils implements ApplicationContextAware {
         Assert.notNull(beanName, "beanName不能为空");
         return (T) applicationContext.getBean(beanName);
     }
+
+
+    /**
+     *
+     * @param beanId 注册的beanId(name)
+     * @param className bean的className，
+     *                     三种获取方式：1、直接书写，如：com.mvc.entity.User
+     *                                   2、User.class.getName
+     *                                   3.user.getClass().getName()
+     */
+    public static void registerBean(String beanId, String className) {
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(className);
+        BeanDefinition beanDefinition = builder.getBeanDefinition();
+        BeanDefinitionRegistry beanDefinitionRegistry = (DefaultListableBeanFactory) ((ConfigurableApplicationContext)applicationContext).getBeanFactory();
+        beanDefinitionRegistry.registerBeanDefinition(className, beanDefinition);
+    }
+
 }
